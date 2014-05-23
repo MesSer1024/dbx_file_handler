7cebf2fe94d6879589cd3284b99608ed2078cd56
Daniel <Dahlkvist>
2014-05-23 08:28:40 +0200

    Initial commit
    Signed-off-by: Daniel <Dahlkvist>

---
+++ dbx_lib/Class1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace dbx_lib
+{
+    public class Class1
+    {
+        public enum DbxType
+        {
+            unknown,
+            SoundConfigurationAsset,
+            SoundPatchConfigurationAsset,
+            UIWidget,
+        }
+
+        public FileInfo[] GetDbxFiles(string path)
+        {
+            var dir = new DirectoryInfo(path);
+            if (!dir.Exists)
+                throw new ArgumentException("Folder does not exist");
+
+            return dir.GetFiles("*.dbx");
+        }
+
+        public DbxType GetDbxType(string filePath)
+        {
+            var file = new FileInfo(filePath);
+            return GetDbxType(file);
+        }
+
+        public DbxType GetDbxType(FileInfo file)
+        {
+            if (!file.Exists)
+                throw new ArgumentException("File does not exist");
+
+            findDbxTypeInFile(new StreamReader(file.FullName));
+            return DbxType.unknown;
+        }
+
+        public class EarliestOccuranceSearchOptions
+        {
+            public FileInfo file;
+            public string lineFilterToApplyRegexTo;
+            public int maxLinesToRead;
+        }
+
+        public class SearchResult
+        {
+            public int lineNumber;
+            public string line;
+            public StringBuilder sb;
+        }
+
+        public SearchResult findEarliestOccurance(StreamReader sr, EarliestOccuranceSearchOptions options)
+        {
+            ML.Assert(options.file.Exists && !String.IsNullOrEmpty(options.lineFilterToApplyRegexTo));
+
+            int lineNr = 0;
+            StringBuilder sb = new StringBuilder();
+            while (lineNr++ < options.maxLinesToRead)
+            {
+                var line = sr.ReadLine().Trim();
+                sb.AppendLine(line);
+                if (line.Contains(options.lineFilterToApplyRegexTo))
+                {
+                    return new SearchResult() { lineNumber = lineNr, line = line, sb = sb };
+                }
+            }
+            return null;
+        }
+        public System.Xml.XmlDocument findXmlObjectContaining(string s)
+        {
+
+        }
+
+        public DbxType findDbxTypeInFile(StreamReader sr)
+        {
+            int lineNr = 0;
+
+
+            //find out guid of "PrimaryInstance"
+            //find the value of "PrimaryInstance"
+
+            var assetGuid = "";
+            var primaryInstanceGuid = "";
+            bool hasPrimaryInstance = false;
+
+            while (!sr.EndOfStream)
+            {
+                lineNr++;
+                var line = sr.ReadLine();
+                if (line.Contains("primaryInstance"))
+                {
+                    var regexGuid = new Regex("[gG]uid=\"{0}\"");
+                    var regexPrimaryInstance = new Regex("[pP]rimaryInstance=\"{0}\"");
+
+                    var guid = regexGuid.Match(line);
+                    var primary = regexPrimaryInstance.Match(line);
+
+                    if (!guid.Success || !primary.Success)
+                        throw new Exception("Foobar");
+
+                    assetGuid = guid.Value;
+                    primaryInstanceGuid = primary.Value;
+                    hasPrimaryInstance = true;
+                }
+                else if (hasPrimaryInstance && line.Contains(primaryInstanceGuid))
+                {
+                    //need to work with xml data
+                }
+            }
+
+            throw new Exception("Foobar!");
+            //return DbxType.unknown;
+        }
+    }
+}
---
+++ dbx_lib/DiceAsset.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbx_lib
+{
+    class DiceAsset
+    {
+        public string FilePath { get; set; }
+        public string Guid { get; set; }
+        public string PrimaryInstance { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public List<string> Parents { get; set; }
+        public List<string> Children { get; set; }
+
+        public DiceAsset()
+        {
+            Parents = new List<string>();
+            Children = new List<string>();
+        }
+
+        public bool addChildUnique(DiceAsset asset)
+        {
+            if (Children.Contains(asset.Guid))
+                return false;
+            Children.Add(asset.Guid);
+            return true;
+        }
+
+        public bool addParentUnique(DiceAsset asset)
+        {
+            if (Parents.Contains(asset.Guid))
+                return false;
+            Parents.Add(asset.Guid);
+            return true;
+        }
+    }
+}
